f60efbb46310e21b67130c658eadd5d1c3c1fb93
Marc Catrisse <marc.catrisse@upc.edu>
2023-11-12 18:16:19 +0100

    Remove unused iam policy

--- src/grafana_stack.tf
+++ src/grafana_stack.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "grafana_stack" {
   associate_public_ip_address = true
   key_name = "vockey"
   user_data = file("init_script.sh")
-  iam_instance_profile = "LabInstanceProfile"
   root_block_device {
     volume_size = 20
     volume_type = "gp3"
